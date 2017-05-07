3f3cb915ff4259d80f361e20d9e2370d2b1d0435
Sebastien CLOLUS <sclolus@e2r12p1.42.fr>
2017-05-07 09:04:22 +0200

    Norme de minishell.h

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,12 +6,12 @@
 /*   By: sclolus <sclolus@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/20 22:56:18 by sclolus           #+#    #+#             */
-/*   Updated: 2017/05/07 06:37:19 by sclolus          ###   ########.fr       */
+/*   Updated: 2017/05/07 09:04:09 by sclolus          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef __MINISHELL_H__
-# define __MINISHELL_H__
+#ifndef MINISHELL_H
+# define MINISHELL_H
 
 # include "libft.h"
 # include <unistd.h>
@@ -29,7 +29,10 @@
 # define SHELL_NAME "minishell: "
 # define GRAMMAR_CHECKSUM 1636786
 
-# define CHECK(x) do{ft_putendl("__________");ft_putendl(#x);ft_putendl("__________");}while(0);
+/*
+**# define CHECK(x) do{ft_putendl("__________");\
+**ft_putendl(#x);ft_putendl("__________");}while(0);
+*/
 
 # define CHARSET_SPECIAL_CHAR "|&<>;*?"
 # define CHARSET_SPECIAL_DELIMITORS "(|&<>; )"
@@ -66,274 +69,286 @@
 # define ERR_MALLOC "Internal memory management failed at: "
 # define ERR_NOFILE "No such file or directory: "
 
-typedef struct s_env t_env;
 typedef uint8_t	t_bool;
-typedef struct	s_shenv t_shenv;
-typedef struct	s_shell	t_shell;
-
-t_shell		*shell;
-
-typedef struct	s_heredoc
+typedef struct		s_heredoc
 {
-	char	*filename;
-	char	*delimiter;
-}				t_heredoc;
+	char			*filename;
+	char			*delimiter;
+}					t_heredoc;
 
-typedef struct	s_shenv
+typedef struct		s_shenv
 {
-	t_env		*env;
-	char		**var;
-	t_bool		*attr;
-	uint32_t	count;
-	t_list		*heredocs;
-	uint32_t	heredocs_index;
-}				t_shenv;
+	struct s_env	*env;
+	char			**var;
+	t_bool			*attr;
+	uint32_t		count;
+	t_list			*heredocs;
+	uint32_t		heredocs_index;
+}					t_shenv;
 
-typedef struct	s_built_in
+typedef struct		s_built_in
 {
-	char	*id;
-	int32_t	(*f)(char **, t_shenv*);
-}				t_built_in;
+	char			*id;
+	int32_t			(*f)(char **, t_shenv*);
+}					t_built_in;
 
-typedef struct	s_env
+typedef struct		s_env
 {
-	char		**env;
-	uint32_t	variable_count;
-}				t_env;
+	char			**env;
+	uint32_t		variable_count;
+}					t_env;
 
-typedef struct	s_process
+typedef struct		s_process
 {
 	struct s_process	*next;
 	struct s_process	*prev;
 	pid_t				pid;
 	pid_t				gpid;
 	char				**argv;
-}				t_process;
+}					t_process;
 
-typedef struct	s_job
+typedef struct		s_job
 {
-	struct s_job	*next;
-	t_parser		*cmd;
-	t_process		*processes;
-	pid_t			pgid;
-}				t_job;
+	struct s_job		*next;
+	t_parser			*cmd;
+	struct s_process	*processes;
+	pid_t				pgid;
+}					t_job;
 
-typedef struct	s_shell
+typedef struct		s_shell
 {
 	struct termios	backup_term;
 	int				terminal;
 	int				interactive;
 	pid_t			shell_pgid;
-}				t_shell;
+}					t_shell;
 
-int32_t		ft_setup_sighandlers(void);
-void		ft_init_shell(void);
+t_shell				*g_shell;
 
-char		*ft_find_command(char *filename, char **path);
-int32_t		ft_is_unbalanced(char *line);
-char		*ft_line_continuation(char *line);
-int32_t		ft_is_line_backslash_terminated(char *line);
+int32_t				ft_setup_sighandlers(void);
+void				ft_init_shell(void);
 
-char		*ft_variable_expansion(char *token, char **env);
+char				*ft_find_command(char *filename, char **path);
+int32_t				ft_is_unbalanced(char *line);
+char				*ft_line_continuation(char *line);
+int32_t				ft_is_line_backslash_terminated(char *line);
 
-char		*ft_find_env(char const **env, char const *variable);
-char		*ft_strjoin_f(char *a, char *b, int32_t mode);
+char				*ft_variable_expansion(char *token, char **env);
 
-uint32_t	ft_is_escaped(char *input, uint32_t index);
-uint32_t	ft_is_quoted(char *input, uint32_t index);
+char				*ft_find_env(char const **env, char const *variable);
+char				*ft_strjoin_f(char *a, char *b, int32_t mode);
 
-t_parser	*ft_get_shell_parser(void);
+uint32_t			ft_is_escaped(char *input, uint32_t index);
+uint32_t			ft_is_quoted(char *input, uint32_t index);
+
+t_parser			*ft_get_shell_parser(void);
+uint64_t			ft_checksum(char *string);
+
+char				**ft_get_env_value(char **env, char *variable);
 
 /*
 ** Lexer
 */
 
-char		**ft_lexer(char *input);
-t_tokens	*ft_get_tokens(char *input);
-void		ft_put_tokens(char **tokens);
-uint32_t	ft_get_tokens_count(char **tokens);
-void		ft_free_tokens(t_tokens *tokens);
-void			ft_add_token_to_list(t_list **token_list
-					     , char *input, uint32_t start, uint32_t i);
-char			**ft_token_list_to_tab(t_list *token_list);
+char				**ft_lexer(char *input);
+t_tokens			*ft_get_tokens(char *input);
+void				ft_put_tokens(char **tokens);
+uint32_t			ft_get_tokens_count(char **tokens);
+void				ft_free_tokens(t_tokens *tokens);
+void				ft_add_token_to_list(t_list **token_list
+									, char *input, uint32_t start, uint32_t i);
+char				**ft_token_list_to_tab(t_list *token_list);
 
 /*
 ** redirections
 */
 
-typedef int32_t (*t_redirec)(t_parser*);
+typedef int32_t	(*t_redirec)(t_parser*);
 
-int32_t		ft_redirections(t_parser *redirect);
-int32_t		ft_output_redirect(t_parser *redirect);
-int32_t		ft_output_append_redirect(t_parser *redirect);
-int32_t		ft_duplicating_input_redirect(t_parser *redirect);
-int32_t		ft_duplicating_output_redirect(t_parser *redirect);
-int32_t		ft_input_redirect(t_parser *redirect);
-int32_t		ft_read_write_redirect(t_parser *redirect);
+int32_t				ft_redirections(t_parser *redirect);
+int32_t				ft_output_redirect(t_parser *redirect);
+int32_t				ft_output_append_redirect(t_parser *redirect);
+int32_t				ft_duplicating_input_redirect(t_parser *redirect);
+int32_t				ft_duplicating_output_redirect(t_parser *redirect);
+int32_t				ft_input_redirect(t_parser *redirect);
+int32_t				ft_read_write_redirect(t_parser *redirect);
 
 /*
 ** Heredocs
 */
 
-int32_t		ft_heredoc_redirect(t_parser *heredoc);
-void		ft_create_heredocs(t_tokens *tokens, t_shenv *shenv);
-int			ft_open_heredoc_file(char *filename);
-char		*ft_get_heredoc_delimiter_index(t_tokens *tokens, uint32_t index);
-int			ft_get_heredoc_index(uint32_t index, t_shenv *shenv);
-void		ft_clear_heredoc(t_list *heredoc);
-void		ft_put_heredocs(t_shenv *shenv);
-void		ft_get_heredoc(t_heredoc *heredoc, uint32_t index, t_shenv *shenv);
-void		ft_get_heredocs(t_shenv *shenv);
-void		ft_put_heredocs_content(t_shenv *shenv);
-void		ft_clear_heredocs(t_shenv *shenv);
-uint32_t	ft_get_cmd_heredoc_count(t_parser *simple_cmd);
-void		ft_delone_heredoc(t_shenv *shenv);
-t_heredoc	*ft_get_t_heredoc_index(uint32_t index, t_shenv *shenv);
+int32_t				ft_heredoc_redirect(t_parser *heredoc);
+void				ft_create_heredocs(t_tokens *tokens, t_shenv *shenv);
+int					ft_open_heredoc_file(char *filename);
+char				*ft_get_heredoc_delimiter_index(t_tokens *tokens
+													, uint32_t index);
+int					ft_get_heredoc_index(uint32_t index, t_shenv *shenv);
+void				ft_clear_heredoc(t_list *heredoc);
+void				ft_put_heredocs(t_shenv *shenv);
+void				ft_get_heredoc(t_heredoc *heredoc
+								, uint32_t index, t_shenv *shenv);
+void				ft_get_heredocs(t_shenv *shenv);
+void				ft_put_heredocs_content(t_shenv *shenv);
+void				ft_clear_heredocs(t_shenv *shenv);
+uint32_t			ft_get_cmd_heredoc_count(t_parser *simple_cmd);
+void				ft_delone_heredoc(t_shenv *shenv);
+t_heredoc			*ft_get_t_heredoc_index(uint32_t index, t_shenv *shenv);
 
 /*
 ** expansions
 */
 
-int32_t		ft_expansions(t_parser *simple_cmd, t_shenv *env);
-void		ft_var_expansion(char **word, t_shenv *shenv);
-void		ft_quote_removal(char **word);
-void		ft_tilde_expansion(char **word, t_shenv *env);
-int32_t		ft_find_file(char *filename, t_env *env);
-char		*ft_get_file_name(char *file);
-char		*ft_get_path_name(char *file);
-void		ft_expansions_cmd_prefix(t_parser *cmd_prefix, t_shenv *shenv);
-void		ft_expansions_cmd_postfix(t_parser *cmd_postfix, t_shenv *shenv);
-void		ft_expansions_io_filename(t_parser *io_filename, t_shenv *shenv);
-void		ft_expansions_io_number(t_parser *io_number, t_shenv *shenv);
-void		ft_expansions_io_redirect(t_parser *io_redirect, t_shenv *shenv);
-void		ft_expansions_io_file(t_parser *io_file, t_shenv *shenv);
+int32_t				ft_expansions(t_parser *simple_cmd, t_shenv *env);
+void				ft_var_expansion(char **word, t_shenv *shenv);
+void				ft_quote_removal(char **word);
+void				ft_tilde_expansion(char **word, t_shenv *env);
+int32_t				ft_find_file(char *filename, t_env *env);
+char				*ft_get_file_name(char *file);
+char				*ft_get_path_name(char *file);
+void				ft_expansions_cmd_prefix(t_parser *cmd_prefix
+										, t_shenv *shenv);
+void				ft_expansions_cmd_postfix(t_parser *cmd_postfix
+										, t_shenv *shenv);
+void				ft_expansions_io_filename(t_parser *io_filename
+										, t_shenv *shenv);
+void				ft_expansions_io_number(t_parser *io_number
+										, t_shenv *shenv);
+void				ft_expansions_io_redirect(t_parser *io_redirect
+										, t_shenv *shenv);
+void				ft_expansions_io_file(t_parser *io_file
+										, t_shenv *shenv);
 
 /*
 ** built_ins
 */
 
-int32_t		ft_built_in_echo(char **argv, t_shenv *shenv);
-int32_t		ft_built_in(char **argv, t_shenv *shenv);
-int32_t		ft_built_in_cd(char **argv, t_shenv *shenv);
-int32_t		ft_built_in_history(char **argv, t_env *env);
-int32_t		ft_built_in_exit(char **argv, t_shenv *shenv);
-int32_t		ft_built_in_setenv(char **argv, t_shenv *shenv);
-int32_t		ft_built_in_env(char **argv, t_shenv *shenv);
-int32_t		ft_built_in_print_env(t_shenv *shenv);
-void		ft_built_in_exec_env_cmd(char **argv
-					 , t_shenv *shenv, t_shenv *exec_env);
-t_shenv		*ft_modify_env(char **argv, uint32_t count, t_shenv *shenv);
-t_shenv		*ft_create_new_shenv(char **argv
-								, uint32_t count);
-int32_t		ft_built_in_unsetenv(char **argv, t_shenv *shenv);
-int32_t		ft_built_in_export(char **argv, t_shenv *shenv);
-int32_t		ft_built_in_unset(char **argv, t_shenv *shenv);
-int32_t		ft_cd_set_oldpath(t_shenv *shenv);
-char		ft_cdpath_search(t_string *curpath, char *op, t_shenv *shenv);
-int32_t		ft_create_path(t_string *dst, char *path, char *op);
-int32_t		ft_get_pwd(t_string *curpath, t_shenv *shenv);
-int32_t		rst_tstring(t_string *str);
-int32_t		ft_check_chdir(t_string *curpath, char *arg, char *cwd
-							, t_shenv *shenv);
+int32_t				ft_built_in_echo(char **argv, t_shenv *shenv);
+int32_t				ft_built_in(char **argv, t_shenv *shenv);
+int32_t				ft_built_in_cd(char **argv, t_shenv *shenv);
+int32_t				ft_built_in_history(char **argv, t_env *env);
+int32_t				ft_built_in_exit(char **argv, t_shenv *shenv);
+int32_t				ft_built_in_setenv(char **argv, t_shenv *shenv);
+int32_t				ft_built_in_env(char **argv, t_shenv *shenv);
+int32_t				ft_built_in_print_env(t_shenv *shenv);
+void				ft_built_in_exec_env_cmd(char **argv
+									, t_shenv *shenv, t_shenv *exec_env);
+t_shenv				*ft_modify_env(char **argv, uint32_t count, t_shenv *shenv);
+t_shenv				*ft_create_new_shenv(char **argv
+									, uint32_t count);
+int32_t				ft_built_in_unsetenv(char **argv, t_shenv *shenv);
+int32_t				ft_built_in_export(char **argv, t_shenv *shenv);
+int32_t				ft_built_in_unset(char **argv, t_shenv *shenv);
+int32_t				ft_cd_set_oldpath(t_shenv *shenv);
+char				ft_cdpath_search(t_string *curpath, char *op
+									, t_shenv *shenv);
+int32_t				ft_create_path(t_string *dst, char *path, char *op);
+int32_t				ft_get_pwd(t_string *curpath, t_shenv *shenv);
+int32_t				rst_tstring(t_string *str);
+int32_t				ft_check_chdir(t_string *curpath, char *arg, char *cwd
+									, t_shenv *shenv);
 /*
 ** Error handling
 */
 
-void		ft_error_exit(uint32_t n, char **str, int32_t exit_status);
-int32_t		ft_error(uint32_t n, char **str, int32_t return_status);
+void				ft_error_exit(uint32_t n, char **str, int32_t exit_status);
+int32_t				ft_error(uint32_t n, char **str, int32_t return_status);
 
 /*
 ** Permissions/files checks
 */
 
-int32_t	ft_check_exec_perm(char *pathname);
-int32_t	ft_check_exec_read(char *pathname);
-int32_t	ft_check_exec_write(char *pathname);
-int32_t	ft_is_dir(char *pathname);
+int32_t				ft_check_exec_perm(char *pathname);
+int32_t				ft_check_exec_read(char *pathname);
+int32_t				ft_check_exec_write(char *pathname);
+int32_t				ft_is_dir(char *pathname);
 
 /*
 ** Shell environnement management
 */
 
-t_shenv		*ft_init_shenv(uint32_t argc, char **env);
-t_shenv		**ft_get_shenv(void);
-t_env		*ft_get_env(t_shenv *shenv);
-char		**ft_find_var(t_shenv *shenv, char *var);
-int32_t		ft_shenv_get_env_count(t_shenv *shenv);
-uint32_t	ft_get_env_count(char **env);
+t_shenv				*ft_init_shenv(uint32_t argc, char **env);
+t_shenv				**ft_get_shenv(void);
+t_env				*ft_get_env(t_shenv *shenv);
+char				**ft_find_var(t_shenv *shenv, char *var);
+int32_t				ft_shenv_get_env_count(t_shenv *shenv);
+uint32_t			ft_get_env_count(char **env);
 
-t_bool		ft_is_var_exported_index(t_shenv *shenv, uint32_t index);
-t_bool		ft_is_var_exported(t_shenv *shenv, char *var);
-t_bool		ft_set_var_to_export(t_shenv *shenv, char *var);
+t_bool				ft_is_var_exported_index(t_shenv *shenv, uint32_t index);
+t_bool				ft_is_var_exported(t_shenv *shenv, char *var);
+t_bool				ft_set_var_to_export(t_shenv *shenv, char *var);
 
-void		ft_modify_var(t_shenv *shenv, char *var);
-void		ft_add_var(t_shenv *shenv, char *var);
-int32_t		ft_unset_var(t_shenv *shenv, char *var);
+void				ft_modify_var(t_shenv *shenv, char *var);
+void				ft_add_var(t_shenv *shenv, char *var);
+int32_t				ft_unset_var(t_shenv *shenv, char *var);
 
-void		ft_free_t_env(t_env *env);
-void		ft_free_t_shenv(t_shenv *shenv);
-void		ft_free_t_shell(void);
+void				ft_free_t_env(t_env *env);
+void				ft_free_t_shenv(t_shenv *shenv);
+void				ft_free_t_shell(void);
 
 /*
 ** Signal Handling
 */
 
-int32_t		ft_setup_sighandlers(void);
-void		ft_reset_signals(void);
-void		ft_ignore_signals(void);
-void		ft_handler_int(int signum);
-void		ft_handler_tstp(int signum);
-void		ft_handler_cont(int signum);
+int32_t				ft_setup_sighandlers(void);
+void				ft_reset_signals(void);
+void				ft_ignore_signals(void);
+void				ft_handler_int(int signum);
+void				ft_handler_tstp(int signum);
+void				ft_handler_cont(int signum);
 
 /*
 ** Miscellaneous Free functions
 */
 
-void	ft_free_argv(char **argv);
+void				ft_free_argv(char **argv);
 
 /*
 ** Processes
 */
 
-void		ft_clear_processes(t_process **processes);
-void		ft_t_process_print(t_process *process);
-void		ft_put_processes_in_foreground(t_process *process, int cont);
+void				ft_clear_processes(t_process **processes);
+void				ft_t_process_print(t_process *process);
+void				ft_put_processes_in_foreground(t_process *process
+											, int cont);
 
 /*
 ** Term setup
 */
 
-int32_t		ft_unset_term(void);
-int32_t		ft_set_term(void);
-int32_t		ft_unset_echo_mode(void);
-int32_t		ft_set_echo_mode(void);
-int32_t		ft_set_canonical_mode(void);
-int32_t		ft_unset_echoctl_mode(void);
+int32_t				ft_unset_term(void);
+int32_t				ft_set_term(void);
+int32_t				ft_unset_echo_mode(void);
+int32_t				ft_set_echo_mode(void);
+int32_t				ft_set_canonical_mode(void);
+int32_t				ft_unset_echoctl_mode(void);
 
 /*
 ** Shell utilities
 */
 
-void	ft_put_shell_in_foreground(void);
-void	ft_init_shell(void);
-void	ft_exit_shell(int exit_status);
+void				ft_put_shell_in_foreground(void);
+void				ft_init_shell(void);
+void				ft_exit_shell(int exit_status);
 
-/* test*/
+/*
+** Execution
+*/
 
-char		**ft_get_argv(t_parser *simple_cmd);
-int32_t		ft_is_built_in(t_parser *parser);
-t_process	*ft_start_process(t_parser *simple_cmd, pid_t gpid, int *stdfd, t_shenv *shenv);
-int32_t		ft_exec_pipeline(t_parser *parser, t_shenv *shenv);
-int32_t		ft_exec_pipe_sequence(t_parser *parser, t_shenv *shenv);
-t_process	*ft_create_pipeline(t_parser *pipe_sequence, t_shenv *shenv);
-int32_t		ft_exec_list(t_parser *parser, t_shenv *shenv);
-int32_t		ft_exec_and_or(t_parser *parser, t_shenv *shenv);
-int32_t		ft_exec_env_assignment(t_parser *parser, t_shenv *shenv);
-int32_t		ft_exec_cmd_prefix(t_parser *cmd_prefix, t_shenv *shenv);
-void		ft_exec_cmd(char **argv, t_shenv *shenv);
-int32_t		ft_exec_simple_cmd(t_parser *simple_cmd, t_shenv *shenv);
-int32_t		ft_exec_command(t_parser *parser, t_shenv *shenv);
-char		**ft_get_env_value(char **env, char *variable);
-t_process	*ft_t_process_add(t_process **processes, t_process *new);
-uint64_t	ft_checksum(char *string);
+char				**ft_get_argv(t_parser *simple_cmd);
+int32_t				ft_is_built_in(t_parser *parser);
+t_process			*ft_start_process(t_parser *simple_cmd, pid_t gpid
+											, int *stdfd, t_shenv *shenv);
+t_process			*ft_t_process_add(t_process **processes, t_process *new);
+int32_t				ft_exec_pipeline(t_parser *parser, t_shenv *shenv);
+int32_t				ft_exec_pipe_sequence(t_parser *parser, t_shenv *shenv);
+t_process			*ft_create_pipeline(t_parser *pipe_sequence
+										, t_shenv *shenv);
+int32_t				ft_exec_list(t_parser *parser, t_shenv *shenv);
+int32_t				ft_exec_and_or(t_parser *parser, t_shenv *shenv);
+int32_t				ft_exec_env_assignment(t_parser *parser, t_shenv *shenv);
+int32_t				ft_exec_cmd_prefix(t_parser *cmd_prefix, t_shenv *shenv);
+void				ft_exec_cmd(char **argv, t_shenv *shenv);
+int32_t				ft_exec_simple_cmd(t_parser *simple_cmd, t_shenv *shenv);
+int32_t				ft_exec_command(t_parser *parser, t_shenv *shenv);
+
 #endif

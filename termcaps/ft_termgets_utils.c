33d29fa57fa80a46cd91d1fbfbe7cd17991d251b
Alexis ALVES <aalves@e2r13p1.42.fr>
2017-05-13 11:57:15 +0200

    kikou

---
+++ termcaps/ft_termgets_utils.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_termgets_utils.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aalves <aalves@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/05/12 15:23:54 by aalves            #+#    #+#             */
+/*   Updated: 2017/05/12 15:24:23 by aalves           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "termcaps.h"
+#include "minishell.h"
+
+void		ft_append_line(char **line, char **final, uint32_t type)
+{
+	char		*tmp;
+	char		*tmp_str;
+
+	if (!*final)
+		*final = ft_strdup(*line);
+	else
+	{
+		tmp = *final;
+		if (type)
+		{
+			if (!(tmp_str = ft_strjoin(*line, "\n")))
+				exit(EXIT_FAILURE);
+			if (!(*final = ft_strjoin_f(*final, tmp_str, 1)))
+				exit(EXIT_FAILURE);
+		}
+		else
+		{
+			if (!(*final = ft_strjoin(*final, *line)))
+				exit(EXIT_FAILURE);
+		}
+		free(tmp);
+	}
+}

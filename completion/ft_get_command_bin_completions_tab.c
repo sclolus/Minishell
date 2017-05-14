438ce1277a3749f3818f30921e8cb049c23d1325
Sebastien CLOLUS <sclolus@e2r13p2.42.fr>
2017-05-14 05:42:17 +0200

    push

--- completion/ft_get_command_bin_completions_tab.c
+++ completion/ft_get_command_bin_completions_tab.c
@@ -6,11 +6,24 @@
 /*   By: aalves <aalves@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/14 01:30:31 by aalves            #+#    #+#             */
-/*   Updated: 2017/05/14 01:30:32 by aalves           ###   ########.fr       */
+/*   Updated: 2017/05/14 05:42:09 by sclolus          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+
+void	ft_put_lst(t_list *lst)
+{
+	ft_putendl("___LIST___");
+	while (lst)
+	{
+		ft_putendl(lst->content);
+		lst = lst->next;
+	}
+	ft_putendl("___END_LIST___");
+}
+
 
 char		**ft_get_command_bin_completions_tab(char *command_prefix,
 														char **path)
@@ -32,8 +45,9 @@ char		**ft_get_command_bin_completions_tab(char *command_prefix,
 		ft_merge_lists(&path_lst, lst);
 		free(tmp);
 	}
+	ft_put_lst(path_lst);
 	tab = ft_lsttotab_completion(path_lst);
-	ft_free_completions_lst(path_lst);
 	tab = ft_get_completions(tab, command_prefix);
+	ft_free_completions_lst(path_lst);
 	return (tab);
 }

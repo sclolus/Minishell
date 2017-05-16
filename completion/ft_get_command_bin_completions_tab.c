633e422d3d9c1a6db7bc16e32045d106f97bbdae
Alexis ALVES <aalves@e2r13p1.42.fr>
2017-05-16 07:15:24 +0200

    okidoki

--- completion/ft_get_command_bin_completions_tab.c
+++ completion/ft_get_command_bin_completions_tab.c
@@ -23,31 +23,31 @@ void		ft_put_lst(t_list *lst)
 	ft_putendl("___END_LIST___");
 }
 
+static void	add_path(t_list **list, char *path, char *command_prefix)
+{
+	char	*tmp;
+	t_list	*lst;
+
+	if (!(tmp = ft_strjoin(path, "/")))
+		exit(EXIT_FAILURE);
+	if (!(tmp = ft_strjoin_f(tmp, command_prefix, 0)))
+		exit(EXIT_FAILURE);
+	if ((lst = ft_completion_add_directory_bin(tmp)))
+		ft_merge_lists(list, lst);
+	free(tmp);
+}
+
 char		**ft_get_command_bin_completions_tab(char *command_prefix,
 														char **path)
 {
 	char		**tab;
-	char		*tmp;
 	t_list		*path_lst;
-	t_list		*lst;
 	uint32_t	i;
 
 	i = 0;
 	path_lst = NULL;
 	while (path[i])
-	{
-		if (!(tmp = ft_strjoin(path[i++], "/")))
-			exit(EXIT_FAILURE);
-		if (!(tmp = ft_strjoin_f(tmp, command_prefix, 0)))
-			exit(EXIT_FAILURE);
-		if (!(lst = ft_completion_add_directory_bin(tmp)))
-		{
-			free(tmp);
-			continue ;
-		}
-		ft_merge_lists(&path_lst, lst);
-		free(tmp);
-	}
+		add_path(&path_lst, path[i++], command_prefix);
 	tab = ft_lsttotab_completion(path_lst);
 	tab = ft_get_completions(tab, command_prefix);
 	ft_free_completions_lst(path_lst);
